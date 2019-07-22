68aa86be1de10e767a4804fc0efb3a1a4f239e47
VladimirIoan <vladimir_3b@hotmail.com>
2019-07-22 17:44:40 +0300

    it is working a little

--- project/_temp/vendorstyles.js
+++
@@ -1,5 +0,0 @@
-﻿import 'bootstrap/scss/bootstrap.scss';
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'flickity/css/flickity.css';
-import 'magnific-popup/dist/magnific-popup.css';
-import 'animate.css/animate.css';
